5e72d3df245187cab56b77145fab895e5cc3a546
spore <spore2050@gmail.com>
2024-09-08 15:01:40 +0800

    add nix dev shell

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ hugo ];
+        };
+      }
+    );
+}
